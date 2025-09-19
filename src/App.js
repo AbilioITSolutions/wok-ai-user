c251506323072e0f95c55e6bc22100f893db19bc
Swethareddy98 <swethareddy.mekala90@gmail.com>
2025-09-19 15:06:08 +0530

    code pushing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Buttons from './Pages/Buttons';
+import LayersClinic from './Pages/LayersClinic';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LayersClinic />
+      <Buttons />
     </div>
   );
 }
